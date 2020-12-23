07cdd57d57d3f021992163555021f5e0d7f007f1
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2020-12-23 14:39:47 -0300

    attendance date creation

--- models/attendance.js
+++ models/attendance.js
@@ -1,10 +1,17 @@
+const moment = require("moment");
 const connection = require("../infra/connection");
 
 class Attendance {
   add(attendance) {
+    const dateCreated = moment().format("YYYY-MM-DD HH:MM:SS");
+    const date = moment(attendance.date, "DD/MM/YYYY").format(
+      "YYYY-MM-DD HH:MM:SS"
+    );
+    const createdAttendance = { ...attendance, dateCreated, date };
+
     const sql = "INSERT INTO attendances SET ?";
 
-    connection.query(sql, attendance, (error, results) => {
+    connection.query(sql, createdAttendance, (error, results) => {
       if (error) {
         console.log(error);
       } else {
